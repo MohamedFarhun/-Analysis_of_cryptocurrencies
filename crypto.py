208a98966c2969e9c59053778a5d3ffea0498a5c
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-10-07 01:33:53 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,26 +1,6 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-from fbprophet import Prophet
-import matplotlib.pyplot as plt
-import datetime as dt
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from pandas.plotting import scatter_matrix
-import math
-import plotly.graph_objs as go
-import statistics as sp
-import scipy.stats as stats
-from scipy.stats.mstats import gmean
-from statsmodels.stats.stattools import jarque_bera
-from scipy.stats import norm
-from scipy.stats import gamma
-from scipy.stats import binom
-from scipy.stats import poisson
-from scipy.stats import expon
-import seaborn as sns
-
 
 st.title('Analysis of cryptocurrencies')
 
