6f5b79eb57904f9cab2fcbc3cafe239ca52e93eb
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-10-09 01:48:18 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -140,3 +140,26 @@ residuals.plot(kind='kde')
 plt.show()
 st.pyplot(plt)
 plt.close()
+
+X = df_bitcoin.values
+size = int(len(X) * 0.80)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+    model = ARIMA(history, order=(5,1,0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
+    history.append(obs)
+    #print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+# plot
+plt.plot(test)
+plt.plot(predictions, color='red')
+plt.show()
+st.pyplot(plt)
+plt.close()
