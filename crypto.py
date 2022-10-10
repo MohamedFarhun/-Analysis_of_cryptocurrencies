968a39d205aba3549d8a98e40e1eded87ecb8699
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-10-10 17:59:38 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -112,7 +112,7 @@ tsmlogdiff.dropna(inplace=True)
 test_stationarity(tsmlogdiff)
 
 crypto_data = {}
-crypto_data['bitcoin'] = pd.read_csv('bitcoin_price.csv', parse_dates=['Date'])
+crypto_data['bitcoin'] = pd.read_csv('bitcoin.csv', parse_dates=['Date'])
 
 df_bitcoin = pd.DataFrame(crypto_data['bitcoin'])
 df_bitcoin = df_bitcoin[['Date','Close']]
